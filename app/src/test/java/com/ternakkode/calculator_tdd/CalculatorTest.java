cf7907d39f0fe1e8516152e5034dac6edb6c9a13
M. Firhan Azmi Nor <muhammadfirhan@student.ub.ac.id>
2020-11-08 08:04:30 +0700

    Refactor - simplify method call using @Before

--- app/src/test/java/com/ternakkode/calculator_tdd/CalculatorTest.java
+++ app/src/test/java/com/ternakkode/calculator_tdd/CalculatorTest.java
@@ -1,5 +1,6 @@
 package com.ternakkode.calculator_tdd;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,83 +9,80 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotEquals;
 
 public class CalculatorTest {
+    Calculator calculatorClass;
+    Main mainClass;
+
+    @Before
+    public void init() {
+        this.calculatorClass = new Calculator();
+        this.mainClass = new Main(new Calculator());
+    }
 
     @Test
     public void testGiven_CalculatorApp_When_Output_Then_PrintValue() {
-        Calculator calculatorClass = new Calculator();
-        assertNotNull(calculatorClass.toString());
+        assertNotNull(this.calculatorClass.toString());
     }
 
     @Test
     public void testGiven_CalculatorApp_When_CancelOrExit_Then_ResetValue() {
-        Calculator calculatorClass = new Calculator();
-        calculatorClass.resetCalculator();
-        assertEquals("0.0",calculatorClass.toString());
+        this.calculatorClass.resetCalculator();
+        assertEquals("0.0",this.calculatorClass.toString());
     }
 
     @Test
     public void testGiven_CalculatorApp_When_AddByTwo_Then_ValueTwoHigherThanBefore() {
-        Calculator calculatorClass = new Calculator();
-        double initialValue = calculatorClass.val;
-        calculatorClass.addOperation(2);
-        assertEquals(String.format("%.1f", initialValue+2),calculatorClass.toString());
+        double initialValue = this.calculatorClass.val;
+        this.calculatorClass.addOperation(2);
+        assertEquals(String.format("%.1f", initialValue+2),this.calculatorClass.toString());
     }
 
     @Test
     public void testGiven_CalculatorApp_When_MinusByTwo_Then_ValueTwoLowerThanBefore() {
-        Calculator calculatorClass = new Calculator();
-        double initialValue = calculatorClass.val;
-        calculatorClass.minusOperation(2);
-        assertEquals(String.format("%.1f", initialValue-2),calculatorClass.toString());
+        double initialValue = this.calculatorClass.val;
+        this.calculatorClass.minusOperation(2);
+        assertEquals(String.format("%.1f", initialValue-2),this.calculatorClass.toString());
     }
 
     @Test
     public void testGiven_CalculatorApp_When_SubtractByTwo_Then_ValueIsHalfThanBefore() {
-        Calculator calculatorClass = new Calculator();
-        double initialValue = calculatorClass.val;
-        calculatorClass.subtractOperation(2);
-        assertEquals(String.format("%.1f", initialValue/2),calculatorClass.toString());
+        double initialValue = this.calculatorClass.val;
+        this.calculatorClass.subtractOperation(2);
+        assertEquals(String.format("%.1f", initialValue/2),this.calculatorClass.toString());
     }
 
     @Test
     public void testGiven_CalculatorApp_When_MultiplyByTwo_Then_ValueIsTwiceThanBefore() {
-        Calculator calculatorClass = new Calculator();
-        double initialValue = calculatorClass.val;
-        calculatorClass.multiplyOperation(2);
-        assertEquals(String.format("%.1f", initialValue*2),calculatorClass.toString());
+        double initialValue = this.calculatorClass.val;
+        this.calculatorClass.multiplyOperation(2);
+        assertEquals(String.format("%.1f", initialValue*2),this.calculatorClass.toString());
     }
 
     @Test
     public void testGiven_CalculatorApp_When_SelectedOperationIsForResetValue_Then_ReturnTrue() {
-        Calculator calculatorClass = new Calculator();
-        assertTrue(calculatorClass.validateOperation("reset"));
+        assertTrue(this.calculatorClass.validateOperation("reset"));
     }
 
     @Test
     public void testGiven_CalculatorApp_When_SelectedOperationIsForCalculation_Then_ReturnTrue() {
-        Calculator calculatorClass = new Calculator();
-        assertTrue(calculatorClass.validateOperation("add 2"));
+        assertTrue(this.calculatorClass.validateOperation("add 2"));
     }
 
     @Test
     public void testGiven_CalculatorApp_When_SelectMathOperation_Then_ChangeCalculatorValue() {
-        Calculator calculatorClass = new Calculator();
-        double initialValue = calculatorClass.val;
+        double initialValue = this.calculatorClass.val;
         String operation = "add";
-        calculatorClass.doMathOperation(operation, 3);
-        assertNotEquals(String.format("%.1f", initialValue), calculatorClass.toString());
+        this.calculatorClass.doMathOperation(operation, 3);
+        assertNotEquals(String.format("%.1f", initialValue), this.calculatorClass.toString());
     }
 
     @Test
     public void testGiven_CalculatorApp_When_CallSplitInputFunction_Then_SplittingInputToArray() {
-        Calculator calculatorClass = new Calculator();
-        String[] splittedInput = calculatorClass.splitInputs("add 2");
+        String[] splittedInput = this.calculatorClass.splitInputs("add 2");
         assertEquals(2, splittedInput.length);
     }
 
     @Test
     public void testGiven_CalculatorApp_When_DependencyInjectionOnCalculator_Then_CanCallCalculatorObject() {
-        Main mainClass = new Main(new Calculator());
-        assertEquals("0.0", mainClass.calculatorService.toString());
+        assertEquals("0.0", this.mainClass.calculatorService.toString());
     }
 }
